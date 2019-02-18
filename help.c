684862faef6a0ee2a382adb52b8205e8c7e2a06a
AjiteshBatraCU <ajitesh.batra@colorado.edu>
2019-02-18 14:47:55 -0700

    Added files for help menu

---
+++ help.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void help()
+{
+    printf("Welcome to the Interactive Memory Management Project\n>>");
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "help.h"
+
+void main()
+{
+    help();
+}
